ec21044351dc5fa7222e8ba2f9c22d14fe31f13d
thibaultKartable <thibault@kartable.fr>
2018-02-19 16:22:37 +0100

    creating list item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { ShoppingListComponent } from './shoppingList/shoppingList.component';
+import { ShoppingItemComponent } from './shoppingList/shoppingItem.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent
+    ShoppingListComponent,
+    ShoppingItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shoppingList/shoppingItem.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'shopping-item',
+    templateUrl: './shoppingItem.component.html',
+    styleUrls: ['./shoppingItem.component.css']
+  })
+  
+  export class ShoppingItemComponent {
+
+    constructor() {
+    
+    }
+  }
+  
