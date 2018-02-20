cd7a060df8eba832d94e6078abf5f84f079676fd
thibaultKartable <thibault@kartable.fr>
2018-02-20 17:08:06 +0100

    adding stuffs

--- src/app/recipeBook/recipe.component.ts
+++ src/app/recipeBook/recipe.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'recipe',
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   })
   
   export class RecipeComponent {
-
+    
     constructor() {
     
     }
