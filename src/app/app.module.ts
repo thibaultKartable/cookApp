69566487398ad756cecc50ebae8aae96ad72606d
qhuit <qhuit@free.fr>
2018-02-20 23:26:07 +0100

    Reviewing header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,9 @@ import { FooterComponent } from './common/footer/footer.component';
 import { ShoppingListComponent } from './shoppingList/shoppingList.component';
 import { ShoppingItemComponent } from './shoppingList/shoppingItem.component';
 import { RecipesComponent } from './recipeBook/recipes.component';
-import { RecipeDetailComponent } from './recipeBook/recipeDetail.component';
-import { RecipeComponent } from './recipeBook/recipe.component';
+import { RecipeDetailComponent } from './recipeBook/recipeDetail/recipeDetail.component';
+import { RecipeComponent } from './recipeBook/recipe/recipe.component';
+import { ShoppingEditComponent } from './shoppingList/shopping-edit/shopping-edit.component';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { RecipeComponent } from './recipeBook/recipe.component';
     ShoppingItemComponent,
     RecipesComponent,
     RecipeDetailComponent,
-    RecipeComponent
+    RecipeComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
